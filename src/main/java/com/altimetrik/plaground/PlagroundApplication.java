4a76e9850de24ebe34da9bcbb218fab24b36d981
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2020-05-16 09:19:19 -0500

    Initial comming springboot starter

---
+++ src/main/java/com/altimetrik/plaground/PlagroundApplication.java
@@ -0,0 +1,13 @@
+package com.altimetrik.plaground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlagroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlagroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/altimetrik/plaground/PlagroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altimetrik.plaground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlagroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
